9d7206735e80a92bce7f91ad3e2fc844165edf6d
Leonardo F. G. Santana <53916135+LeonardoSantan@users.noreply.github.com>
2024-06-24 12:58:36 -0300

    AJUSTE TIRAR DESCRIÇÃO MAIN

--- bd2/produto.js
+++ bd2/produto.js
@@ -30,3 +30,33 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch((error) => console.log(error));
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const url = `https://smsfranciscobeltrao.com.br/produtos/${localStorage.getItem('currentProduto')}/`
+    axios.defaults.headers.user = localStorage.getItem("id");
+    axios.get(url)
+        .then((response) => {
+            console.log(response.data);
+            const data = response.data;
+
+            let nome = document.querySelector('#titulo');
+
+            let grupo = document.querySelector('#grupo');
+            let preco = document.querySelector('#preco');
+            let quantidade = document.querySelector('#quantidade');
+            let fornecedor = document.querySelector('#fornecedor');
+            let descricao = document.querySelector('#descricao');
+
+            let imagem = document.querySelector('#imagem');
+
+            nome.innerHTML = data.nome;
+
+            grupo.innerHTML = data.grupo;
+            preco.innerHTML = data.valor;
+            quantidade.innerHTML = data.quantidade;
+            fornecedor.innerHTML = data.fornecedor;
+            descricao.innerHTML = data.descricao
+
+            imagem.src = data.imagem;
+        })
+        .catch((error) => console.log(error));
+});
